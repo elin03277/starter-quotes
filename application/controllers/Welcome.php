c65915e0b32c1a5b042010b6e8c3f5caee91b738
Eric Lin <elin03277@gmail.com>
2017-09-16 10:57:11 -0700

    bug fixes 3

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,50 +1,37 @@
 <?php
-
 defined('BASEPATH') OR exit('No direct script access allowed');
-
 class Welcome extends Application
 {
-
-	function __construct()
-	{
-		parent::__construct();
-	}
-
-	/**
-	 * Homepage for our app
-	 */
-	public function index()
-	{
-		// this is the view we want shown
-		$this->data['pagebody'] = 'homepage';
-
-		// build the list of authors, to pass on to our view
-		$source = $this->quotes->all();
-
-		// pass on the data to present, as the "authors" view parameter
-		$this->data['authors'] = $source;
-
-		$this->render();
-	}
-
+    function __construct()
+    {
+        parent::__construct();
+    }
     /**
-     * Show just one actor 
+     * Homepage for our app
+     */
+    public function index()
+    {
+        // this is the view we want shown
+        $this->data['pagebody'] = 'homepage';
+        // build the list of authors, to pass on to our view
+        $source = $this->quotes->all();
+        // pass on the data to present, as the "authors" view parameter
+        $this->data['authors'] = $source;
+        $this->render();
+    }
+    /**
+     * Show just one actor
      */
     public function show($key)
     {
         // this is the view we want shown
         $this->data['pagebody'] = 'actor';
-
         // build the list of authors, to pass on to our view
         $source = $this->quotes->get($key);
-
         // pass on the data to present, as the "authors" view parameter
         $this->data['authors'] = $source;
-
         // pass on the data to present, adding the author record's fields
         $this->data = array_merge($this->data, (array) $source);
-
         $this->render();
     }
-
 }
